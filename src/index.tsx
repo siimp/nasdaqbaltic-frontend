757e06f3dd867c8d40166d25867049e9733ed7b9
siim puustusmaa <siimpuustusmaa@gmail.com>
2018-09-30 16:43:47 +0300

    css cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'node_modules/bulma/css/bulma.min.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
   <App />,
